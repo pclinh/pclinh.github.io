514f967fe070366d7a34899459cc10363b6c762c
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-22 16:54:57 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,7 +7,14 @@ window.fbAsyncInit = function() {
   }); 
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-  });
+});
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 $("#Get_btn").click(function(){
     $.get(response.authResponse.url+"access_token="+response.authResponse.accessToken, function(data2, status){
       console.log(data2)
