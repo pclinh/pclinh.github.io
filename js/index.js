65906211a1157024c94bcee06c52ecc3c01279f2
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-22 20:18:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,9 +21,9 @@ window.fbAsyncInit = function() {
  function statusChangeCallback(response){
    if(response.status === 'connected'){
     console.log('Logged in and authenticated');
-    var access_token =$.get("https://graph.facebook.com/102135788849157?fields=access_token&access_token="+response.authResponse.accessToken, function(data, status){
+    var access_token =$.get("https://graph.facebook.com/102135788849157?fields=access_token&access_token="+response.authResponse.accessToken, function(data.access_token, status){
 
-    }).access_token;
+    });
     var url1 = 'https://graph.facebook.com/v11.0/102135788849157/feed?';
     $("#Get_btn").click(function (){ 
        $.get(url1+"access_token="+access_token, function(data2, status){
