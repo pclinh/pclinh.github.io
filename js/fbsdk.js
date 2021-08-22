55d9fab3ae3bae2daf5cfd31a98a092d809d64ce
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-22 12:21:56 +0700

    Add files via upload

---
+++ js/fbsdk.js
@@ -0,0 +1,38 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1229103174227463',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v11.0'
+    });
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+  };
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   function statusChangeCallback(response){
+     if(response.status === 'connected'){
+      console.log('Logged in and authenticated');
+        window.location.href="main.html"
+     } else {
+       console.log('Not authenticated');
+     }
+   }
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+  function logout (){
+        FB.logout(function(response){
+      });
+    }
