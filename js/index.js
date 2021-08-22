f882dd7cd66ca877aacbbf28e921b8cd6cb22462
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-22 17:02:23 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,21 +1,41 @@
-console.log('updated')
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '1229103174227463',
     cookie     : true,
     xfbml      : true,
     version    : 'v11.0'
-  }); 
+  });
+
+  FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+  });
+};
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+ function statusChangeCallback(response){
+   if(response.status === 'connected'){
+    console.log('Logged in and authenticated');
+   } else {
+     console.log('Not authenticated');
+   }
+ }
+
+function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-});
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  });
+}
+function logout (){
+      FB.logout(function(response){
+          window.location.href='index.html'
+    });
+  }
 $("#Get_btn").click(function(){
     $.get(response.authResponse.url+"access_token="+response.authResponse.accessToken, function(data2, status){
       console.log(data2)
@@ -26,4 +46,3 @@ $("#Get_btn").click(function(){
      });
     });
   })
-}
