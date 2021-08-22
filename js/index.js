c8341dec6bcb6f2e4a32f4de67380b62f67332fc
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-23 00:33:46 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ window.fbAsyncInit = function() {
         url: 'https://graph.facebook.com/102135788849157?fields=access_token&access_token='+response.authResponse.accessToken, 
         data: { get_param: 'value' }, 
         success: function (data) {
-          access_token = data.access_token
+          access_token = data.access_token.value
           
         },
     });
