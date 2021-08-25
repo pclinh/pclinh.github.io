dbf3a148bea8625bb4948012d06bfd797996cf8b
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-25 18:28:27 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-console.log('10');
+console.log('1');
 var access_token='';
 var url1 = 'https://graph.facebook.com/v11.0/102135788849157/feed?';
 window.fbAsyncInit = function() {
@@ -60,12 +60,12 @@ $("#Get_btn").click(function (){
   $("#show").replaceWith('<p id="show"></p>');
   $.get(url1+"access_token="+access_token, async function(data2, status){
     console.log(data2)
-   $.each(data2.data, function( index, value ) {      
-   await var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'"+value.id+"\'>delete</button></td></tr>");
-    document.getElementById(value.id).addEventListener('click',()=>{
-    console.log('clicked')
+   await $.each(data2.data, function( index, value ) {      
+    var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'"+value.id+"\'>delete</button></td></tr>");
     })
     $("#show").append(row);
+    document.getElementById(value.id).addEventListener('click',()=>{
+    console.log('clicked')
    });
   });
 });
