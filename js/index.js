07b37d28393b8cbf389a0d66ddf7454339c6fd5c
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-26 11:11:23 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-console.log('2');
+console.log('3');
 var access_token='';
 var url = 'https://graph.facebook.com/v11.0/'
 var url1 = 'https://graph.facebook.com/v11.0/102135788849157/feed?';
@@ -56,7 +56,8 @@ $("#Get_btn").click(function (){
    await $.each(data2.data, function( index, value ) {      
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'"+value.id+"\'>delete</button></td></tr>");
     $("#show").append(row);
-    document.getElementById(value.id).addEventListener('click',()=>{async function deletePost(){ 
+    document.getElementById(value.id).addEventListener('click',()=>{async function deletePost(){
+          console(url+value.id+"?access_token="+access_token);
           $.ajax({
             url: 'url+value.id+"?access_token="+access_token',
             method: 'DELETE',
