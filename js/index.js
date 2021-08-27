a4a55657887d9046b7cb7f72c58d2244263c29bc
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-27 15:11:05 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ function checkLoginState() {
 }
 $("#Get_btn").click(function get_clicked(){ 
   $("#show").replaceWith('<p id="show"></p>');
-  $.get(url1+"access_token="+access_token+"&fields=id,permalink_url,message,created_time, async function(data2, status){
+  $.get(url1+"access_token="+access_token+"&fields=id,permalink_url,message,created_time", async function(data2, status){
    await $.each(data2.data, function( index, value ) {
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"delete_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     $("#show").append(row);
