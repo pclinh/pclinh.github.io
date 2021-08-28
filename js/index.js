93ccaaff444e68618b30594d1ada5ee26352f1d8
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-28 13:14:07 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,11 +52,11 @@ $("#Get_btn").click(function get_clicked(){
      console.log(value.permalink_url);
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     $("#show").append(row);
-      document.getElementById("update_"+value.id).addEventListener('click',()=>{
+      document.getElementById("update_"+value.id).addEventListener('click',async ()=>{
             var content = "<div class=\'fb-post\' data-href=\'"+value.permalink_url +"\' data-width=\'500\'>12</div>";
             console.log(content);
-            $("#showPost").replaceWith(content);
-             $("#showPost").load();
+            await $("#showPost").replaceWith(content);
+             $("#showPost").reload();
       })
           document.getElementById("delete_"+value.id).addEventListener('click',()=>{
           rurl=url+value.id+"?access_token="+access_token; 
