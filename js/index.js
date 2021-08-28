94b8d7e12d822e05be112371d4ded7a7d17ad58f
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-28 14:06:40 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,6 +56,8 @@ $("#Get_btn").click(function get_clicked(){
             var content = "<div class=\'fb-post\' data-href=\'"+value.permalink_url +"\' data-width=\'500\'>12</div>";
             console.log(content);
             await $("#showPost").replaceWith(content);
+                  $(".refresh").load();
+                   
 
       })
           document.getElementById("delete_"+value.id).addEventListener('click',()=>{
