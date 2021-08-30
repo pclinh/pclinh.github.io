671e0cbb07af8f169e061ff25b28df2f6cdc2be7
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-30 15:54:17 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -53,10 +53,8 @@ $("#Get_btn").click(function get_clicked(){
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     $("#show").append(row);
             document.getElementById("update_"+value.id).addEventListener('click',()=>{
-            //var content = "<div class='fb-post' id='showPost' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
-            $(#showPost).setAttribute('class', 'fb-post' );
-            $(#showPost).setAttribute('data-href', value.permalink_url);
-            $(#showPost).setAttribute('data-width', 500);
+            var content = "<div class='fb-post' id='showPost' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
+            $(#showPost).replaceWith(content);
             $(#showPost).load(function(){
               console.log("succeed")
             });
