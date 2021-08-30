78bcee683453ce7d3dbd97f8ee429bb4ff3408cf
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-30 18:34:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,14 +52,9 @@ $("#Get_btn").click(function get_clicked(){
      console.log(value.permalink_url);
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     $("#show").append(row);
-            document.getElementById("update_"+value.id).addEventListener('click',async ()=>{
-            var div = document.createElement('div');
-            div.setAttribute('id', 'test' );
-            div.setAttribute('class', 'fb-post' );
-            div.setAttribute('data-href', value.permalink_url);
-            div.setAttribute('data-width', 500);
-            // var content = "<div class='fb-post' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
-            $("#showPost").load(" "+div);
+            document.getElementById("update_"+value.id).addEventListener('click',()=>{
+             var content = " <div class='fb-post' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
+            $("#showPost").load("",content);
             });
      
           document.getElementById("delete_"+value.id).addEventListener('click',()=>{
