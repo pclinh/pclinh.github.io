bf4ae507ecaba35a6a68c6d2340dba6f9231f4c7
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-31 15:38:31 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,15 +57,9 @@ $("#Get_btn").click(function get_clicked(){
            document.getElementById("update_"+value.id).addEventListener('click',()=>{
              var content = "<div class='fb-post' data-show-text='true' data-href='"+value.permalink_url +"' data-width='500'></div>";
              window.location.href='./show.html';
-             $(document).ready(async function(){
-               console.log(content);
-             await function addattr (){
                       $("#showPost").setAttribute('class', 'fb-post' );
                       $("#showPost").setAttribute('data-href', value.permalink_url);
                       $("#showPost").setAttribute('data-width', 500);
-             }
-             });
-          
             });
      
           document.getElementById("delete_"+value.id).addEventListener('click',()=>{
