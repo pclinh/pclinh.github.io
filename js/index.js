1319c5cb38e1260f21c07645bf2b253a27b47892
pclinh <62631357+pclinh@users.noreply.github.com>
2021-08-31 14:08:02 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,8 +52,13 @@ $("#Get_btn").click(function get_clicked(){
      console.log(value.permalink_url);
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td>&emsp;&emsp;Content:&emsp;"+value.message + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     var content = " <div class='fb-post' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
-    $("#show").append(row); 
-    $("#showPost").html(content);
+    $("#show").append(row);
+    $.ajax({
+      type: "POST",
+      url: value.permalink_url
+    }).done(function(data) {
+      $("#LoadPages").load(content);
+    });
 
            /* document.getElementById("update_"+value.id).addEventListener('click',()=>{
              var content = " <div class='fb-post' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
