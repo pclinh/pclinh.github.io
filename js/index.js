f84e652de72631e770d6be0a8c6bd4c3f74d1a28
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-05 12:48:04 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -52,12 +52,12 @@ $("#Get_btn").click(function get_clicked(){
      var limitW = 10;
       //Số ký tự của từ
       var char = 4;
-      var txt = value.message;
-      var txtStart = txt.slice(0,limitW).replace(/\w+$/,'');
+      var showtxt = value.message;
+      var txtStart = showtxt.slice(0,limitW).replace(/\w+$/,'');
         console.log(txtStart);
-      var txtEnd = txt.slice(txtStart.length);
+      var txtEnd = showtxt.slice(txtStart.length);
       if ( txtEnd.replace(/\s+$/,'').split(' ').length > char ) {
-        var showtxt = textStart+"<a>...more</a>";
+          showtxt = textStart+"<a>...more</a>";
       }
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td class='show_content'id='Show_"+value.id+"'>&emsp;&emsp;Content:&emsp;"+showtxt + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     idshow = "'#Show_"+value.id+"'";
