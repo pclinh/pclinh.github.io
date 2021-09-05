8e6ab095c1a4349451d4d5946f4db87581cc9177
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-05 12:25:46 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,17 +55,6 @@ $("#Get_btn").click(function get_clicked(){
      console.log(idshow);
      var content = " <div class='fb-post' data-href='"+value.permalink_url +"' data-width='500'>12</div>";
     $("#show").append(row);
-    function detail(){
-       var limitW = 10;
-      //Số ký tự của từ
-      var char = 4;
-      var txt = $(idshow).html();
-      var txtStart = txt.slice(0,limitW).replace(/\w+$/,'');
-      var txtEnd = txt.slice(txtStart.length);
-      if ( txtEnd.replace(/\s+$/,'').split(' ').length > char ) {
-        $(idshow).html(textStart+"<a>...more</a>");
-    }    
-    };
            document.getElementById("update_"+value.id).addEventListener('click',()=>{
              var content = "<div class='fb-post' data-show-text='true' data-href='"+value.permalink_url +"' data-width='500'></div>";
              function setAttr(){
@@ -74,8 +63,6 @@ $("#Get_btn").click(function get_clicked(){
               document.querySelector("#showPost").setAttribute('data-width', 500);
              }
              window.location.href='./show.html';
-           
-                      
             });
      
           document.getElementById("delete_"+value.id).addEventListener('click',()=>{
@@ -91,6 +78,19 @@ $("#Get_btn").click(function get_clicked(){
         })
       });
    });
+  function detail(){
+       var limitW = 10;
+      //Số ký tự của từ
+      var char = 4;
+      var txt = $(".show_content").html();
+      console.log(txt)
+      var txtStart = txt.slice(0,limitW).replace(/\w+$/,'');
+        console.log(txtStart);
+      var txtEnd = txt.slice(txtStart.length);
+      if ( txtEnd.replace(/\s+$/,'').split(' ').length > char ) {
+        $(".show_content").html(textStart+"<a>...more</a>");
+    }    
+    };
 });
 
 
