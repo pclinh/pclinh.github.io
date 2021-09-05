e84926ccda639ca793c91287a9d773666b34d629
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-05 13:04:45 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,7 +57,7 @@ $("#Get_btn").click(function get_clicked(){
         console.log(txtStart);
       var txtEnd = showtxt.slice(txtStart.length);
       if ( showtxt > limitW ) {
-          showtxt = textStart+"'<a>...more</a>'";
+          showtxt = txtStart+"'<a>...more</a>'";
       }
     var row = $("<tr><td>&emsp;&emsp;ID:&emsp;"+value.id + "</td><td>&emsp;&emsp;Created time:&emsp; "+value.created_time+ "</td><td class='show_content'id='Show_"+value.id+"'>&emsp;&emsp;Content:&emsp;"+showtxt + "</td><td><button class=\"delete_btn\" id=\'delete_"+value.id+"\'>delete</button></td><td><button class=\"update_btn\" id=\'update_"+value.id+"\'>update</button></td></tr>");
     idshow = "'#Show_"+value.id+"'";
