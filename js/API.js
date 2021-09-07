6ddf2c99947b4aea97dafea33be2dfa911424bec
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-07 17:59:46 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -101,11 +101,11 @@ $("#post_btn").click(async function (){
         console.log(e);
       }) 
   }else{
-   let url1="https://graph.facebook.com/v11.0/102135788849157/photo?"
+   let url1="https://graph.facebook.com/v11.0/102135788849157/photos?"
    message=document.getElementById("post_content").value;
    photo_url= $("#photo_upload").val();
    console.log(photo_url);
-    await $.post(url1+"access_token="+access_token+"&message="+message+"&url="+photo_url, function(data2, status){
+    await $.post(url1+"access_token="+access_token+"&message="+message+"&source="+photo_url, function(data2, status){
       alert('Post succeed');
       }).catch(e =>{
         console.log(e);
