02d6ae5a8bc57d103d2a3bebd7e9227509bb4d28
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-16 14:25:56 +0700

    Add files via upload

---
+++ js/post.js
@@ -0,0 +1,20 @@
+if($("#photo_upload").prop('files').length==0){ 
+    message=document.getElementById("post_content").value;
+    FB.api(
+  '/102135788849157/feed',
+  'POST',
+  {"message":"123\n"},
+  function(response) {
+      // Insert your code here
+  }
+);      
+  }else{
+    message=document.getElementById("post_content").value;
+    FB.api(
+    '/102135788849157/photos',
+    'POST',
+    {"message":message},
+    function(response) {
+        // Insert your code here
+    })
+}
