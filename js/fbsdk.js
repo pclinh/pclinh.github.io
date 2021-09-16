4174d494c0eec95891c4237fe2a7ef0f87dad332
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-16 15:49:36 +0700

    Update fbsdk.js

--- js/fbsdk.js
+++ js/fbsdk.js
@@ -1,38 +1,48 @@
 var access_token="";
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1229103174227463',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v11.0'
-    });
+window.fbAsyncInit = function(){
+  FB.init({
+    appId      : '1229103174227463',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v11.0'
+  });
 
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-  };
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-   function statusChangeCallback(response){
-     if(response.status === 'connected'){
-      console.log('Logged in and authenticated');
-     } else {
-       console.log('Not authenticated');
-       window.location.href='index.html'
-     }
-   }
-
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
+  FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
-    });
-  }
+     
+  });
+};
+ (function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+ async function statusChangeCallback(response){
+   if(response.status === 'connected'){
+    console.log('Logged in and authenticated');
+     await  $.ajax({ 
+        type: 'GET', 
+        url: 'https://graph.facebook.com/102135788849157?fields=access_token&access_token='+response.authResponse.accessToken, 
+        data: { get_param: 'value' },
+        success: function (data) {
+         access_token=data.access_token;
+        },
+    }).catch(e => {
+    console.log(e);
+});
+   } else {
+     console.log('Not authenticated');
+     window.location.href='index.html';
+   }
+ }
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
   function logout (){
         FB.logout(function(response){
             window.location.href='index.html'
