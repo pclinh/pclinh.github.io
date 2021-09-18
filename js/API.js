4f2dfef15e11ec6aa49f7bb7c28fd6997b5af4ec
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-18 14:48:26 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -84,7 +84,7 @@ $("#post_btn").click(async function(){
     {
     "message":document.getElementById("post_content").value,
     "access_token":access_token,
-    "url":blAsDataUrl
+    "url":blobAsDataUrl
     },
     function(response) {
        console.log(response)
