51701a59ec209a63207891ac2918bf3118be22d5
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-19 14:42:11 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -69,7 +69,7 @@ $("#post_btn").click(async function(){
 	formData.append('access_token', access_token);
 	formData.append('message',message);
 	
-	forEach($("#photo_upload").files,function(item){
+	$("#photo_upload").files.forEach(item=>{
 	const fileReader = new FileReader();
 	const file = item;
 	const photoData = new Blob([fileReader.result], {type: 'image/*'});
