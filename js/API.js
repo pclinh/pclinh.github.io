f2c829afe8c09fbdd0aeeb53e0dbb49355b0c68d
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-21 14:13:54 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -83,7 +83,7 @@ $("#post_btn").click(async function(){
 		}
 	});
 	
-	promiseA.then(()=> Ơlet response = fetch(`https://graph.facebook.com/102135788849157/photos`,{
+	promiseA.then(()=>{let response = fetch(`https://graph.facebook.com/102135788849157/photos`,{
 		body: formData,
 		method: 'post'
 	});
@@ -91,5 +91,5 @@ $("#post_btn").click(async function(){
    			console.log(value);
 		}
 		console.log(response);
-  )}
+  )}}
 });
