b383e0540b95385f465fdbd677a43b117971774c
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-22 20:48:01 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -14,7 +14,7 @@ $("#get_btn").click(function get_clicked(){
       if (response && !response.error){
       await $.each(response.data,function (index, value){
       var limitW = 10;
-      var showtxt=await value.message;
+      var showtxt=value.message;
       var char = 4;
       var txtStart = value.message.slice(0, limitW);
       var txtEnd = value.message.slice(txtStart.length);
