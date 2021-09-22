c92e728abde50204f898f797b46a2629cf2c9781
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-22 15:54:20 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -69,7 +69,7 @@ $("#post_btn").click(async function () {
     var formData = new FormData();
     formData.append('access_token', access_token);
     formData.append('message', message);
-    const prom = new Promise(async (resolve, reject) =>{
+    const await prom = Promise(async (resolve, reject) =>{
       try {
         for (let i = 0; i < $("#photo_upload").prop('files').length; i++) {
           const fileReader = new FileReader();
