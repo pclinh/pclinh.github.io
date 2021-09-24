df1f2c2e07c62336dc5cab1d68c2452cba656b08
pclinh <62631357+pclinh@users.noreply.github.com>
2021-09-24 17:43:40 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -76,7 +76,7 @@ $("#post_btn").click(async function() {
       }
     );
   } else {
-          await fetch("https://graph.facebook.com/102135788849157/album",{
+          await fetch("https://graph.facebook.com/102135788849157/photos",{
           body: formData,
           method: "post",
         })
