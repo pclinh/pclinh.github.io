5bcf85e928254ea70ac9bad03365f156ad2b9c04
pclinh <62631357+pclinh@users.noreply.github.com>
2021-10-02 15:21:12 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ async function show(messagetxt){
   var showtxt = await messagetxt;
   var limitW = 3;
   var txtStart = showtxt.slice(0, limitW);
-  if (showtxt > limitW){
+  if (showtxt.lenght() > limitW){
       showtxt = await txtStart+ "...";
      }
   return showtxt;
