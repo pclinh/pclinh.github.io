b81dc1e457958a74f151fe1d895cb2d8fc56b0ad
pclinh <62631357+pclinh@users.noreply.github.com>
2021-10-08 15:47:47 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $.getScript("./js/fbsdk.js");
 console.log("getted1")
-var formData;
-var type;
+
+
 async function show(messagetxt){
   var showtxt = await messagetxt;
   var limitW = 3;
@@ -13,28 +13,7 @@ async function show(messagetxt){
      }
   return showtxt;
 }
-$("#photo_upload").change(function(){
-formData = new FormData();
-  if($("#photo_upload").prop("files").length > 1){
-        multi=true;
-  }else{
-        multi=false
-  }
-for (let i = 0; i < $("#photo_upload").prop("files").length; i++){
-    const fileReader = new FileReader();
-    const file = document.getElementById("photo_upload").files[i];
-    fileReader.onload = () =>{
-       type= file.type;
-       const data = new Blob([fileReader.result],{name:".mp4",},{type: type,});
-        var fileOfBlob = new File([data], '.mp4')
-        formData.append("source"+i, fileOfBlob);
-          for (var value of formData.values()) {
-               console.log(value);
-            }
-        };
-        fileReader.readAsArrayBuffer(file);
-        }
-})
+
 function create_time(date){
 var d = new Date(date)
 dt = d.toLocaleDateString('en-GB');
