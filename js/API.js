4b7ac71f89d10d9a6aaf7821544025c5cb8d0fc5
pclinh <62631357+pclinh@users.noreply.github.com>
2021-10-12 13:53:01 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -53,12 +53,13 @@ $("#get_btn").click(function get_clicked(){
       document.getElementById(value.id).addEventListener('click', () =>{
         $(".selected_post").removeClass("selected_post")
         $("#"+value.id).addClass("selected_post");
-         perma=
+         $("#postdtail").replace(<div id="postdtail"></div>);
              FB.api(
                  '/oembed_post',
                    'GET',
                   {"url":value.permalink_url,"maxwidth":"600","useiframe":"true"},
               function(response) {
+               
                   $("#postdtail").append(response.html);
                }
             );
