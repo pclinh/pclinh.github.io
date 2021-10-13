38ad046bec84e93d55a700f67ce2013ef4ee291a
pclinh <62631357+pclinh@users.noreply.github.com>
2021-10-13 23:40:15 +0700

    Update API.js

--- js/API.js
+++ js/API.js
@@ -51,8 +51,8 @@ $("#get_btn").click(function get_clicked(){
       var row = $("<div class='select_post' id="+value.id+">"+showtxt+"<div class='select_post_createtime'> "+showtime+"</div></div>");
       await $("#post_selection").append(row)
       document.getElementById(value.id).addEventListener('click', () =>{
-          show_post(value.id, value.permalink_url)
-          cmt(value.id,value.permalink_url);
+      await show_post(value.id, value.permalink_url)
+      await cmt(value.id,value.permalink_url);
       });
        /*
      document.getElementById("delete_" + value.id).addEventListener('click', () =>{
