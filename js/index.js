504fe11cb59a670774838d3febdf7eeaa53619fd
pclinh <62631357+pclinh@users.noreply.github.com>
2021-10-18 13:40:01 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,31 @@ function show_post(id, permalink){
             );
   $("iframe").css("background-color","whitesmoke");
 }
+function cmt_filter(id,permalink_url,content){
+  FB.api(
+   id+"/",
+  'GET',
+   {"fields":"comments",
+    "access_token":access_token},
+  async function(response){
+    $("#comment_detail").replaceWith("<div id='comment_detail'></div>")
+    if (response && !response.error){
+   await $.each(response.comments.data,async function (index, value){
+    if(value.message.search(content)!=-1){  
+    var cmt_detail="<div class=cmt_link id="+value.id+">"+value.from.name +": " + value.message+"</div>";
+     $("#comment_detail").append(cmt_detail);
+    }
+      document.getElementById(value.id).addEventListener('click',async () =>{
+           const cmt_id= value.id.slice(value.id.search("_")+1,value.id.length)
+           console.log(cmt_id)
+           const cmt_url= permalink_url+"&comment_id="+cmt_id;
+                   window.open(cmt_url);
+      });
+        }
+      );
+    }}
+  );
+}
 function cmt(id,permalink_url){               
   FB.api(
    id+"/",
